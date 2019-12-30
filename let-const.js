31005c8e7b85291c717a38c4e556cdaa0cdac728
That JS Dude <khan4019@gmail.com>
2019-12-30 08:28:28 -0800

    let const

---
+++ let-const.js
@@ -0,0 +1,11 @@
+const wife = 'Arian Grande';
+console.log(wife);
+
+let potential = 'Bruno Mars';
+potential = 'Justin Bieber';
+console.log(potential);
+
+for(let i = 0; i<5;i++){
+    console.log(i);
+}
+console.log("outside of the loop", i);

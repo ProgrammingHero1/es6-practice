5d346ab31492ddbdbb639100e95a5052bf1fa97f
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2021-08-22 23:05:47 -0500

    Add files via upload

---
+++ es6-practice.js
@@ -0,0 +1,20 @@
+/*
+1. let const
+2. template string
+2.a: use variable
+2.b: use object  property
+3. arrow function
+3.a. with one parameter. will return the number after dividing by 5
+3.b. with two parameters. will add two to each number and then multiply the result.
+3.c. three parameters. will multiply all three parameters
+3.d. with two parameters but it will be a multi line arrow function. will add two to each number and then multiply the result.
+make sure you return the result
+4. [home work]: javascript function declaration vs arrow function
+5. array of numbers. and then map to get each elements multiplied by 5. must use arrow function
+6. [challenging] numbers of array. get odd numbers by using filter with arrow function
+7. [challenging] array of objects (e.g. products). use find to get the object with price 5000.
+8. You have an object. Now use destructing to create a simple variable of any property of the object.
+9. [challenging] You have an array. Now use destructing to create a simple (or more) variable to get the third element of the array in a variable called 'three'.
+10. [optional] just a write a function with three parameters. and the last parameter will have a default parameter with value 7.
+this function will take three parameters and will return the sum of all the three parameters.
+*/
